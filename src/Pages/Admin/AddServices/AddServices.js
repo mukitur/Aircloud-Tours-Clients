510cc1c531d53531026c28bd17a86cd4f633dfef
mukitur <mukit.inway@gmail.com>
2021-10-29 14:08:18 +0600

    react hook form implemented

--- src/Pages/Admin/AddServices/AddServices.js
+++ src/Pages/Admin/AddServices/AddServices.js
@@ -1,10 +1,10 @@
 //import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
-//import './AddServices.css';
 
 const AddServices = () => {
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit } = useForm();
+
     const onSubmit = data => {
         console.log(data)
     }
@@ -12,7 +12,12 @@ const AddServices = () => {
     return (
         <div className="addServices">
             <h2 className="my-5">Please Add Packages</h2>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Package Name" /><br/>
+                <textarea {...register("description")} placeholder="Package Description"/><br/>
+                <input type="price" {...register("Price")} placeholder="Price" /><br/>
+                <input type="submit" value="Add Packages"/>
+            </form>
         </div>
     );
 };
