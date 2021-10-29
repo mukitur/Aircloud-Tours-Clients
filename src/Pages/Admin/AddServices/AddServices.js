e214ad262c652fd565542a60bd59f0ab2466ed1d
mukitur <mukit.inway@gmail.com>
2021-10-29 13:27:06 +0600

    work on add services

--- src/Pages/Admin/AddServices/AddServices.js
+++ src/Pages/Admin/AddServices/AddServices.js
@@ -1,9 +1,15 @@
+//import axios from 'axios';
 import React from 'react';
+//import { useForm } from "react-hook-form";
+//import './AddServices.css';
 
 const AddServices = () => {
+    
+
     return (
-        <div>
-            <h2>This is AddServices Page</h2>
+        <div className="addServices">
+            <h2 className="my-5">Please add services</h2>
+            
         </div>
     );
 };
