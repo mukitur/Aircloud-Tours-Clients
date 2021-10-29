b5bb00e9d425deb86d18b029ad57f5c397b1def8
mukitur <mukit.inway@gmail.com>
2021-10-29 13:52:37 +0600

    works on add services

--- src/Pages/Admin/AddServices/AddServices.js
+++ src/Pages/Admin/AddServices/AddServices.js
@@ -1,14 +1,17 @@
 //import axios from 'axios';
 import React from 'react';
-//import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 //import './AddServices.css';
 
 const AddServices = () => {
-    
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    }
 
     return (
         <div className="addServices">
-            <h2 className="my-5">Please add services</h2>
+            <h2 className="my-5">Please Add Packages</h2>
             
         </div>
     );
