ee13c69fbcff2039739fd1e570aa26057f629250
mukitur <mukit.inway@gmail.com>
2021-10-31 13:36:08 +0600

    login ui change

--- src/Pages/Login/Login/Login/Login.js
+++ src/Pages/Login/Login/Login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router';
 import useAuth from '../../../../hooks/useAuth';
 import logo from '../../../../images/logo1.png';
 import googleIcon from '../../../../images/google.png';
+import image from '../../../../images/banner3.jpg';
 
 const Login = () => {
     const {signInWithGoogle } = useAuth();
@@ -21,8 +22,16 @@ const Login = () => {
     return (
         <div>
              <img className = "mt-4" src = {logo} alt=""/>
-             <Container className="mt-5 border">
-                <Button onClick={handleGoogleLogin} variant="light"><img className = "mt-4" src = {googleIcon} alt=""/> Login with GOOGLE </Button>
+             <Container>
+                <Row className="text-start my-5">
+                    <Col className="bg-light p-5" sm={6}>
+                        <h4 className="text-success">Please Login using your Gmail Account! </h4>
+                        <Button onClick={handleGoogleLogin} variant="light"><img className = "mt-4" src = {googleIcon} alt=""/>  </Button>
+                    </Col>
+                    <Col sm={6} className="text-center">
+                        <img rounded src = {image} width={"100%"} alt=""/>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
