846133c6d810142af652e674fee52051f96ae422
mukitur <mukit.inway@gmail.com>
2021-10-31 14:17:11 +0600

    ui update

--- src/Pages/Login/Login/Login/Login.js
+++ src/Pages/Login/Login/Login/Login.js
@@ -22,17 +22,15 @@ const Login = () => {
     return (
         <div>
              <img className = "mt-4" src = {logo} alt=""/>
-            <div className="container px-4 my-5">
+            <div className="container px-4">
                 <div className="row gx-5">
-                    <div className="col">
-                        <div className="p-3 border bg-light">
+                    <div className="col col-md-12">
+                        <div className="p-3 border bg-light my-5">
                             <h4 className="text-success">Please Login using your Gmail Account! </h4>
                             <Button onClick={handleGoogleLogin} variant="light"><img className = "mt-4" src = {googleIcon} alt=""/>  </Button>
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="p-3 border bg-light"><img className="text-right" rounded src = {image} width={"100%"} alt=""/></div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
